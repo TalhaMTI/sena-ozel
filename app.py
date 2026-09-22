127ecd523749a2d1637bfe0fcb19f3b6cc0f7aac
TalhaMTI <muhammedtalhaisilci@gmail.com>
2026-09-22 16:05:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import streamlit as st
+
+# Sayfaya tarayıcının manifest olarak algılayacağı meta etiketi ekleyelim
+st.markdown(
+    """
+    <head>
+        <link rel="manifest" href="data:application/manifest+json;charset=utf-8,{
+            'name': 'Sena\'ya Özel',
+            'short_name': 'Sena',
+            'start_url': './',
+            'display': 'standalone',
+            'background_color': '#0e1117',
+            'theme_color': '#0e1117',
+            'icons': []
+        }">
+    </head>
+""",
+    unsafe_allow_html=True,
+)
+
+import streamlit as st
 import os
 from datetime import datetime
 
